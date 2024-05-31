a1b37fdf2925c51368ec322b73bdb8f06c0853e0
Martin Vala <rangwath@gmail.com>
2024-05-31 14:50:43 +0200

    Add trail behind ships and cannonballs

--- Assets/Scripts/Misc/DestroyUtil.cs
+++ Assets/Scripts/Misc/DestroyUtil.cs
@@ -2,8 +2,19 @@ using UnityEngine;
 
 public class DestroyUtil : MonoBehaviour
 {
+    [SerializeField] private TrailRenderer trailRenderer;
+
     public void DestroyThisGameObject()
     {
         Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        if (trailRenderer != null)
+        {
+            trailRenderer.transform.SetParent(null);
+            Destroy(trailRenderer.gameObject, trailRenderer.time + 1f);
+        }
     }
 }
